84dfd5267f4a764f7eb0ddb193ab668ee4d290c7
Johan Roux <roux.jm@gmail.com>
2023-11-20 16:34:14 +0200

    Updating files

--- GitCommitPush.bat
+++ GitCommitPush.bat
@@ -1,7 +1,26 @@
 @echo off
 
-git add -A
-git commit -m "Updating..."
-git push
+echo Commit and Push to GitHub
+echo -------------------------
 
+set /p message= Please enter the update message: 
+
+echo Message: "%message%"
+
+:continue_github
+set /P answer=Commit and Push to GitHub [Y/N]?
+if /I "%answer%" EQU "Y" goto :process_github
+if /I "%answer%" EQU "N" goto :now_quit
+goto :continue_github
+
+:process_github
+git add -A
+git commit -m "%message%"
+git push
 pause
+exit
+
+:now_quit
+echo Aborted
+pause
+exit
